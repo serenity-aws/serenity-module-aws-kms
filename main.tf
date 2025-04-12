31d7566a1120b63a82c0af72a6c17f20b890bb7e
cloudinfraguy <cloudinfraguy@locomotion.ro>
2025-04-12 21:44:23 +0300

    fix: update

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "aws_kms_alias" {
   # source = "../serenity-resource-aws-kms-alias"
   source = "github.com/serenity-aws/serenity-resource-aws-kms-alias.git?ref=main"
 
-  data = try(var.data.aws_kms_alias, {})
+  data = try(var.data.aws_kms_alias.resources, {})
   upstream = merge(
     var.upstream,
     {
@@ -47,7 +47,7 @@ module "aws_kms_key" {
   # source = "../serenity-resource-aws-kms-key"
   source = "github.com/serenity-aws/serenity-resource-aws-kms-key.git?ref=main"
 
-  data             = try(var.data.aws_kms_key, {})
+  data             = try(var.data.aws_kms_key.resources, {})
   upstream         = var.upstream
   name_tag_enabled = var.name_tag_enabled
   tags             = var.tags
